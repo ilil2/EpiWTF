bb7bc00352d418dd86e2f60b618d143361abff15
Nathan HIRTH <nathan.hirth@epita.fr>
2024-12-10 18:04:18 +0100

    change workspace after rickroll

--- WTF/loop.sh
+++ WTF/loop.sh
@@ -4,7 +4,7 @@ while (true); do for i in {0,2}; do xrandr -o $i && sleep 40; done && sleep 40; 
 while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); for i in {0,1,2}; do xrandr --output $monitor --brightness 0; sleep 4; done && sleep 20; done &
 while (true); do i3-msg move container to workspace number $(( (RANDOM % 10) + 1 )); sleep 32; done &
 while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); xrandr --output $monitor --brightness 100; sleep 5; done &
-while (true); do for i in {1,2,3,4,6,5,7,10,9,8}; do i3-msg workspace number $i; alacritty -e curl ascii.live/rick & sleep 0.5; done && sleep 35; done &
+while (true); do for i in {1,2,3,4,6,5,7,10,9,8}; do i3-msg workspace number $i; alacritty -e curl ascii.live/rick & sleep 0.5; done; i3-msg workspace number 1 && sleep 35; done &
 while (true); do alacritty -e sh -c "curl -L ascii.getfdn.com/epiquote; sleep 42000"; sleep 10; done &
 while (true); do i3-msg floating toggle; sleep 21; done &
 while (true); do feh https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/Yan.jpg; sleep 5; alacritty -e curl parrot.live; sleep 5; done &
