4761adcc26ddc845093285642dcfb39489573af5
Nathan HIRTH <nathan.hirth@epita.fr>
2024-12-10 16:48:55 +0100

    Add gamma change

--- WTF/loop.sh
+++ WTF/loop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while (true); do mouse=($(xinput | grep Mouse | cut -d = -f2 | cut -d [ -f1)); xinput disable $mouse; sleep 3; xinput enable $mouse; sleep 15; done &
-while (true); do for i in {2,0}; do xrandr -o $i && sleep 30; done && sleep 120; done &
+sleep 15; while (true); do for i in {2,0}; do xrandr -o $i && sleep 30; done && sleep 120; done &
 while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); for i in {0,1,2}; do xrandr --output $monitor --brightness 0; sleep 7; done && sleep 65; done &
 while (true); do i3-msg move container to workspace number $(( (RANDOM % 10) + 1 )); sleep 32; done &
 while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); xrandr --output $monitor --brightness 100; sleep 10; done &
@@ -8,3 +8,4 @@ while (true); do for i in {1,2,3,4,6,5,7,10,9,8}; do i3-msg workspace number $i;
 while (true); do alacritty -e curl -L ascii.getfdn.com/epiquote; sleep 14; done &
 while (true); do i3-msg floating toggle; sleep 12; done &
 while (true); do feh https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/Yan.jpg; sleep 5; alacritty -e curl parrot.live; sleep 23; done &
+while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); R=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+1); print min+rand()*(max-min)}'); G=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+2); print min+rand()*(max-min)}'); B=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+3); print min+rand()*(max-min)}'); xrandr --output $monitor --gamma $R:$G:$B; sleep 2; done &
