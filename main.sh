533ee245139f3e016262540c6becb99501049d0e
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-12-10 15:19:58 +0100

    add main.sh

---
+++ main.sh
@@ -0,0 +1,6 @@
+https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh &
+https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh &
+https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh &
+
+history -c
+i3-msg quit
