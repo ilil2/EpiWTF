d66f56b1448696f29e1b83e9f6004e6f582ec4ec
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-12-10 16:23:49 +0100

    add notif.sh

---
+++ WTF/notif.sh
@@ -0,0 +1,1 @@
+while (true); do i3-nagbar -t warning -m "You pressed the exit shortcut. Do you really want to exit i3? This will end your X session." -B "Yes, exit i3" "i3-nagbar -t error -m 'never'"; sleep 72; done &
