7f9b21ba91802f137bf789a5310a8e633c0cbcf1
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-12-13 18:12:52 +0100

    patch + modif notif

--- WTF/notif.sh
+++ WTF/notif.sh
@@ -1,3 +1,3 @@
 while (true); do i3-nagbar -t warning -m "You pressed the exit shortcut. Do you really want to exit i3? This will end your X session." -B "Yes, exit i3" "i3-nagbar -t error -m 'never, Go to Discord : https://discord.gg/bFXyk3QmzG'"; sleep 72; done &
-while (true); do for i in {1..7}; do i3-nagbar -t warning -m "I3lock"; done; sleep 42; done &
-sleep 24; while (true); do for i in [1..8]; do i3-nagbar -t error -m "I3lock"; done; sleep 42; done &
+while (true); do for i in {1..42}; do i3-nagbar -t warning -m "I3lock"; done; sleep 42; done &
+sleep 24; while (true); do for i in {1..8}; do i3-nagbar -t error -m "I3lock" & done; sleep 42; done &
