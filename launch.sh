223a3024557442c9b95f7ce2685e0f4834467d93
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-12-13 18:33:58 +0100

    add debug bat

--- launch.sh
+++ launch.sh
@@ -1,6 +1,15 @@
+i3-nagbar -t warning -m "1"
 while [ "$(/nix/store/*xprintidle*/bin/xprintidle)" -le "10000" ]
-while [ "$(/nix/store/*xprintidle*/bin/xprintidle)" -ge "10000" ]
+do
+	sleep 0.1
+done
 
-(sleep 30; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh) &
-(sleep 35; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh) &
-(sleep 25; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh) &
+i3-nagbar -t warning -m "2"
+while [ "$(/nix/store/*xprintidle*/bin/xprintidle)" -ge "10000" ]
+do
+	sleep 0.1
+done
+
+(sleep 30; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/loop.sh | sh -s) &
+(sleep 35; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/notif.sh | sh -s) &
+(sleep 25; curl https://raw.githubusercontent.com/ilil2/EpiWTF/main/WTF/img.sh |sh -s) &
